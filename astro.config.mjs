e16451a87ef7b779a39039e9be6279e7ecf0c600
simayi <hong-geonhui@hong-geonhuiui-MacBookAir.local>
2023-05-01 22:42:16 +0900

    second commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,11 @@ export default defineConfig({
   // used to generate images
   site:
     process.env.VERCEL_ENV === 'production'
-      ? 'https://brutal.elian.codes/'
+      ? 'https://github.com/si-ma-yi/blog'
       : process.env.VERCEL_URL
       ? `https://${process.env.VERCEL_URL}/`
       : 'https://localhost:3001/',
+  base: process.env.VERCEL_ENV === 'production' ? '/blog' : '',
   trailingSlash: 'ignore',
   integrations: [sitemap(), UnoCSS({ injectReset: true })],
 });
