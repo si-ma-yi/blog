4a5db690bde7e79b3253ffc079aadb08a159eed4
simayi <dev.hong.89@gmail.com>
2023-05-02 00:12:40 +0900

    modify

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,11 @@ export default defineConfig({
   // used to generate images
   site:
     process.env.VERCEL_ENV === 'production'
-      ? 'https://si-ma-yi.github.io'
+      ? 'https://si-ma-yi.github.io/blog'
       : process.env.VERCEL_URL
       ? `https://${process.env.VERCEL_URL}/`
       : 'https://localhost:3001/',
-  base: process.env.VERCEL_ENV === 'production' ? '/blog' : '',
+  // base: process.env.VERCEL_ENV === 'production' ? '/blog' : '',
   trailingSlash: 'ignore',
   integrations: [sitemap(), UnoCSS({ injectReset: true })],
 });
