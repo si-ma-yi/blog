224391af13e491a0074f537713d3c24ba3a4c912
simayi <dev.hong.89@gmail.com>
2023-05-02 00:29:25 +0900

    sdfsadF

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
       : process.env.VERCEL_URL
       ? `https://${process.env.VERCEL_URL}/`
       : 'https://localhost:3001/',
-  base: process.env.VERCEL_ENV === 'production' ? '/blog' : '',
+  base: process.env.VERCEL_ENV === 'production' ? '../' : '',
   trailingSlash: 'ignore',
   integrations: [sitemap(), UnoCSS({ injectReset: true })],
   output: 'static',
