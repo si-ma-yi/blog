95240ad97f7a75f6adc7bb031c613240c5c90be9
simayi <dev.hong.89@gmail.com>
2023-05-02 00:37:58 +0900

    sdfsadF

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,19 @@ import UnoCSS from 'unocss/astro';
 
 export default defineConfig({
   // used to generate images
+  
   site:
     process.env.VERCEL_ENV === 'production'
       ? 'https://si-ma-yi.github.io'
       : process.env.VERCEL_URL
       ? `https://${process.env.VERCEL_URL}/`
       : 'https://localhost:3001/',
-  base: process.env.VERCEL_ENV === 'production' ? '../' : '',
+  base: process.env.VERCEL_ENV === 'production' ? '/blog' : '',
+  
   trailingSlash: 'ignore',
   integrations: [sitemap(), UnoCSS({ injectReset: true })],
-  output: 'static',
+  build: {
+    assets: '_astro',
+    assetsPrefix: 'https://si-ma-yi.github.io/blog'
+  }
 });
