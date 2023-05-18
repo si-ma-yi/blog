423d6b575e4bcc633ae620ca14b52e79d98cff97
hong <gh.hong@saigeresearch.ai>
2023-05-18 21:30:38 +0900

    루트 패스 수정

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,12 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import UnoCSS from "unocss/astro";
 
-console.log("ENV : ", process.env);
+console.log(
+  "ENV : ",
+  process.env.npm_lifecycle_script === "astro build"
+    ? "https://si-ma-yi.github.io"
+    : "https://localhost:3001/"
+);
 export default defineConfig({
   // used to generate images
 
