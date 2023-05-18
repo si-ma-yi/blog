b3a41005198bb473705829f38d1a2fa13bdb0d8c
hong <gh.hong@saigeresearch.ai>
2023-05-18 21:26:59 +0900

    루트 패스 수정

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,15 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import UnoCSS from "unocss/astro";
 
+console.log("ENV : ", process.env);
 export default defineConfig({
   // used to generate images
 
   site:
-    process.env.VERCEL_ENV === "production"
+    process.env.npm_lifecycle_script === "astro build"
       ? "https://si-ma-yi.github.io"
-      : process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/`
       : "https://localhost:3001/",
-  base: process.env.VERCEL_ENV === "production" ? "/blog" : "",
+  base: process.env.npm_lifecycle_script === "astro build" ? "/blog" : "",
 
   trailingSlash: "ignore",
   integrations: [sitemap(), UnoCSS({ injectReset: true })],
