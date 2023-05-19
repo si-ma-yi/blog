198b3c774480c46575e71ffb67341336bd05d0ea
hong <gh.hong@saigeresearch.ai>
2023-05-19 09:20:14 +0900

    테스트

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,12 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import UnoCSS from "unocss/astro";
 
-console.log(
-  "ENV : ",
-  process.env.npm_lifecycle_script === "astro build"
-    ? "https://si-ma-yi.github.io"
-    : "https://localhost:3001/"
-);
 export default defineConfig({
   // used to generate images
 
   site:
     process.env.npm_lifecycle_script === "astro build"
-      ? "https://si-ma-yi.github.io"
+      ? "https://si-ma-yi.github.io/blog"
       : "https://localhost:3001/",
   base: process.env.npm_lifecycle_script === "astro build" ? "/blog" : "",
 
