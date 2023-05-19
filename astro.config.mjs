97a15327293bc7defb38b230145dddc06986f160
hong <gh.hong@saigeresearch.ai>
2023-05-19 11:15:54 +0900

    수정

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,14 +8,13 @@ export default defineConfig({
   site:
     process.env.npm_lifecycle_script === "astro build"
       ? "https://si-ma-yi.github.io/blog"
-      : "http://127.0.0.1:3000",
-  base: process.env.npm_lifecycle_script === "astro build" ? "/blog" : "",
+      : "http://127.0.0.1:3000/blog",
+  base: process.env.npm_lifecycle_script === "astro build" ? "/blog" : "/blog",
 
   trailingSlash: "ignore",
   integrations: [sitemap(), UnoCSS({ injectReset: true })],
   build: {
-    build: "file",
     assets: "_astro",
-    assetsPrefix: "https://si-ma-yi.github.io/blog",
+    assetsPrefix: "https://si-ma-yi.github.io/blog/",
   },
 });
