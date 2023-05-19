129ba5e741e93fbe5875e52851cd45bd19e4c734
hong <gh.hong@saigeresearch.ai>
2023-05-19 10:35:53 +0900

    테스트

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ export default defineConfig({
 
   site:
     process.env.npm_lifecycle_script === "astro build"
-      ? "https://si-ma-yi.github.io/blog"
-      : "https://localhost:3001/",
+      ? "https://si-ma-yi.github.io"
+      : "http://127.0.0.1:3000",
   base: process.env.npm_lifecycle_script === "astro build" ? "/blog" : "",
 
   trailingSlash: "ignore",
